12bccb39469d73087a061c87e6407ec13b749b23
JuanDGiraldoM <jgmarin@bancolombia.com.co>
2023-05-15 07:21:10 -0500

    Remove comments

--- lib/inventory_web/controllers/sales_json.ex
+++ lib/inventory_web/controllers/sales_json.ex
@@ -28,21 +28,6 @@ defmodule InventoryWeb.SalesJSON do
         Decimal.add(acc, order.amount)
       end)
 
-    #    products_resume =
-    #      products
-    #      |> Enum.reduce(%{}, fn product, acc ->
-    #        Map.put(acc, product.name, %{
-    #          quantity: product.quantity,
-    #          amount: Decimal.mult(product.unit_price, product.quantity)
-    #        })
-    #      end)
-    #
-    #    total =
-    #      products
-    #      |> Enum.reduce(Decimal.new("0"), fn product, acc ->
-    #        Decimal.add(acc, Decimal.mult(product.unit_price, product.quantity))
-    #      end)
-    #
     response = %{
       products: product_sales,
       total: total
